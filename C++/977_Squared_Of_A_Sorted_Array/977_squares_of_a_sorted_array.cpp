3847cad7205f6448a57b9d82c7ebdfeb8a53d581
Ivan <ivananshukoff@gmail.com>
2019-10-29 18:44:45 +0300

    Added 977 solution

---
+++ C++/977_Squared_Of_A_Sorted_Array/977_squares_of_a_sorted_array.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution{
+public:
+    std::vector<int> sortedSquares(std::vector<int>& A);
+};
+
+//std::vector<int>Solution::sortedSquares(std::vector<int>& A){
+//    std::vector<int> resultVec;
+//    for(auto a : A){
+//        resultVec.push_back(a*a);
+//    }
+//    std::sort(resultVec.begin(), resultVec.end());
+//    return resultVec;
+//}
+
+std::vector<int>Solution::sortedSquares(std::vector<int>& A){
+    std::vector<int> hash(10001,0);
+    for(auto a : A){
+        ++hash[std::abs(a)];
+    }
+    std::vector<int> resVec;
+    for(int i = 0; i <=10000; ++i){
+        while(hash[i] != 0){
+            resVec.push_back(i*i);
+            --hash[i];
+        }
+    }
+    return resVec;
+}
+
+
+int main(void){
+    std::vector<int> vec = {-7, -3, 2, 3, 11};
+    Solution solution;
+    std::vector<int> resultVec = solution.sortedSquares(vec);
+    for(auto a : resultVec){
+        std::cout << a << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
+
