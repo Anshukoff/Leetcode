884c8ea2977ee69e044fea4eb5c893b3c65cb431
Ivan <ivananshukoff@gmail.com>
2019-10-31 20:17:24 +0300

    Added 1207 solution

---
+++ C++/1207_Unique_Number_Of_Occurrences/1207_Unique_Number_Of_Occurrences.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+class Solution{
+public:
+    bool uniqueOccurrences(std::vector<int>& arr);
+};
+
+bool Solution::uniqueOccurrences(std::vector<int>& arr){
+    int a[2001] = {0};
+    for(auto x : arr){
+        a[x+1000]++;
+    }
+    std::set<int> st;
+    for(int i = 0; i < 2000; ++i){
+        if(a[i] && st.find(a[i]) != st.end()){
+            return false;
+        }
+        else if(a[i]){
+            st.insert(a[i]);
+        }
+    }
+    return true;
+}
+
+int main(void){
+    Solution solution;
+    std::vector<int> vec = {1, 2, 2, 1, 1, 3};
+    std::cout << solution.uniqueOccurrences(vec) << std::endl;
+    return 0;
+}
